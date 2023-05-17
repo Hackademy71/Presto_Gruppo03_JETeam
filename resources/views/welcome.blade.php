ee86b5760fd377fea5291d08cc75205d84356774
MelaniaAbitabile <melania.abitabile20@gmail.com>
2023-05-17 12:56:01 +0200

    ok, va bene

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,16 @@
 <x-layout>
-    <div class="container-fluid hero-image">
+    <div class="container-fluid mt-5">
         <div class="row">
-            <div class="col-sm-12 ps-0">
-                
-                    <img src="./img/online.png" alt="img-header">
-                
-                {{-- <div class="hero-text">
-                    <h1 class="f-p fw-bold fs-1 justify-content-end text-gradient-50"> {{__('ui.welcome')}}</h1>
+            <div class="col-sm-12 ps-0 hero-image">
+
+              
+
+                <div class="hero-text text-end h-100">
+                    <h1 class="f-p fw-bold fs-1 justify-content-end text-gradient-50"> {{ __('ui.welcome') }}</h1>
                     <p class="f-p fs-2 justify-content-center text-gradient-50">
-                        {{__('ui.slogan')}}
+                        {{ __('ui.slogan') }}
                     </p>
-                </div> --}}
+                </div>
             </div>
 
             <div class="container-fluid">
@@ -34,25 +34,8 @@
                         </div>
                     @endforeach
 
-                    {{-- @foreach ($announcements as $announcement)
-                        <div class="col-3 mt-3 mb-3  ">
-                            <div class="card mb-3 bordo" style="width: 18rem;">
-                                <img src="https://picsum.photos/200" class="card-img-top" alt="">
-                                <div class="card-body">
-                                    <h5 class="card-title f-p">{{ $announcement->name }}</h5>
-                                    <p class="card-text f-s">{{ $announcement->description }}</p>
-                                    <p class="card-text f-s">{{ $announcement->price }} €</p>
-                                    <p class="card-text f-s">Aggiunto il
-                                        {{ $announcement->created_at->format('d/m/Y') }}</p>
-                                    <a href="{{ route('detArticle', compact('announcement')) }}"
-                                        class="btn bgmy4 f-p m-3">Visualizza dettaglio</a>
-                                    <a href="{{ route('categoryShow', ['category' => $announcement->category]) }}"
-                                        class="btn bgmy4 f-s m-3">Categoria: {{ $announcement->category->name }}</a>
-                                </div>
-                            </div>
-                        </div>
-                    @endforeach --}}
                 </div>
             </div>
-
+        </div>
+    </div>
 </x-layout>
