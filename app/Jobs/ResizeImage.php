6a99ee6b97101d34f0bf6d7ede1fb87414d51750
al.hackcod.do <al.code.do@gmail.com>
2023-05-18 17:42:39 +0200

    prova watermark

--- app/Jobs/ResizeImage.php
+++ app/Jobs/ResizeImage.php
@@ -36,7 +36,7 @@ class ResizeImage implements ShouldQueue
         $destPath = storage_path() . '/app/public/' . $this->path . "/crop_{$w}x{$h}_" . $this->fileName;
 
         $croppedImage = Image::load($srcPath) 
-        ->crop(Manipulations::CROP_CENTER, $w, $h)
-        ->save($destPath);
+        ->crop(Manipulations::CROP_CENTER, $w, $h)->watermark( storage_path() . '/app/public/img/logo.png')
+        ->watermarkOpacity(50)->watermarkPadding(10, 10, Manipulations::UNIT_PERCENT)->save($destPath);
     }
 }
