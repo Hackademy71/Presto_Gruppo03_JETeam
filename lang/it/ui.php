0331a69cbfb52f1a1b9da3a992000bc06055c6a0
MelaniaAbitabile <melania.abitabile20@gmail.com>
2023-05-19 16:15:15 +0200

    traduzione iniziata

--- lang/it/ui.php
+++ lang/it/ui.php
@@ -3,4 +3,18 @@
 return [
     'welcome' => 'Benvenuti su Presto.it!',
     'slogan' => 'Il tuo marketplace preferito!',
+    'area-utente' => 'Area Utente',
+    'login' => 'Accedi',
+    'registrati' => 'Registrati',
+    'profilo-utente' => 'Profilo Utente',
+    'logout' => 'Esci',
+    'area-revisore' => 'Area Revisore',
+    'annunci-da-revisionare' => 'Annunci da revisionare',
+    'recheck' => "Annunci revisionati",
+    'annunci' => 'Annunci',
+    'tutti-gli-annunci' => 'Tutti gli annunci',
+    'inserisci-annuncio' =>'Inserisci annuncio',
+    'categorie' => 'Categorie',
+    'lingue' => 'Lingue',
+    'lavora' => 'Lavora con noi',
 ];
