d9909ddb38c18aaa2f15016aa6a77a32499db6cc
felixian87 <126794124+felixian87@users.noreply.github.com>
2023-05-19 16:11:28 +0200

    USER STORY8 fatta

--- app/Http/Livewire/NewArticle.php
+++ app/Http/Livewire/NewArticle.php
@@ -2,14 +2,15 @@
 
 namespace App\Http\Livewire;
 
-use App\Jobs\GoogleVisionLabelImage;
-use App\Jobs\GoogleVisionSafeSearch;
 use Livewire\Component;
 use App\Models\Category;
+use App\Jobs\RemoveFaces;
 use App\Jobs\ResizeImage;
-
 use App\Models\Announcement;
+
 use Livewire\WithFileUploads;
+use App\Jobs\GoogleVisionLabelImage;
+use App\Jobs\GoogleVisionSafeSearch;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\File;
 
@@ -69,9 +70,13 @@ class NewArticle extends Component
                 //$this->announcement->images()->create(['path'=>$image->store('images','public')]);
                 $newFileName= "announcements/{$this->announcement->id}";
                 $newImage = $this->announcement->images()->create(['path'=>$image->store($newFileName, 'public')]);
-                dispatch(new ResizeImage($newImage->path, 400 ,300));
-                dispatch(new GoogleVisionSafeSearch($newImage->id));
-                dispatch(new GoogleVisionLabelImage($newImage->id));
+                RemoveFaces::withChain([
+                new ResizeImage($newImage->path, 400 ,300),
+                new GoogleVisionSafeSearch($newImage->id),
+                new GoogleVisionLabelImage($newImage->id),
+
+                ])->dispatch($newImage->id);
+                
             }
              File::deleteDirectory(storage_path('/app/livewire-tmp'));
         }
