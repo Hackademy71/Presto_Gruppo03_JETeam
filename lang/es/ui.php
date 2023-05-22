cc98159a3dfb54ebaeb5e98eee2d11536c0b275d
felixian87 <126794124+felixian87@users.noreply.github.com>
2023-05-22 14:37:31 +0200

    modifiche

--- lang/es/ui.php
+++ lang/es/ui.php
@@ -3,4 +3,18 @@
 return [
     'welcome' => 'Benvenido su Presto.it!',
     'slogan' => 'Tu mercado favorito',
+    'area-utente' => 'Tu cuenta personal',
+    'login' => 'Iniciar sesión',
+    'registrati' => 'Registrati',
+    'profilo-utente' => 'Mi cuenta personal',
+    'logout' => 'Esci',
+    'area-revisore' => 'Area Revisore',
+    'annunci-da-revisionare' => 'Annunci da revisionare',
+    'recheck' => "Annunci revisionati",
+    'annunci' => 'Annunci',
+    'tutti-gli-annunci' => 'Tutti gli annunci',
+    'inserisci-annuncio' =>'Inserisci annuncio',
+    'categorie' => 'Categorie',
+    'lingue' => 'Lingue',
+    'lavora' => 'Lavora con noi',
 ];
