9ec22054fc4b87a5696f00a8319494a670d88db2
MelaniaAbitabile <melania.abitabile20@gmail.com>
2023-05-22 16:27:11 +0200

    traduzione

--- lang/en/ui.php
+++ lang/en/ui.php
@@ -2,5 +2,20 @@
   
 return [
     'welcome' => 'Welcome on Presto.it!',
-    'slogan' => 'Your favorite marketplace'
+    'slogan' => 'Your favorite marketplace',
+    'area-utente' => 'Your account',
+    'login' => 'Login',
+    'registrati' => 'Register',
+    'profilo-utente' => 'User profile',
+    'logout' => 'Logout',
+    'area-revisore' => 'Workspace',
+    'annunci-da-revisionare' => 'Ads to',
+    'recheck' => "Recheck",
+    'annunci' => 'Announcements',
+    'tutti-gli-annunci' => 'All announcements',
+    'inserisci-annuncio' =>'Add announcement',
+    'categorie' => 'Departments',
+    'lingue' => 'Language',
+    'lavora' => 'Work with us',
+    'dove-siamo' => 'Where we are'
 ];
